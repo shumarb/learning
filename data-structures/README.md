98a168974bdfd82a33b402bb89259811abc4f0b7
Sheikh-Umar <shumarb@outlook.com>
2023-10-29 08:35:23 +0800

    Updated README on ArrayList & Vector, added folder on ArrayList

--- data-structures/README.md
+++ data-structures/README.md
@@ -2,17 +2,18 @@
 
 This folder contains my summary of structures.
 
-The following illustrates the worst-case time complexity of operations involving `N` elements (where `N >= 1`) in each data structure:
+The following illustrates the worst-case time complexity for operations on each of the `N` elements (where `N >= 1`) in each data structure:
 
 Data Structure           | Insertion               | Deletion                | Search                   | Search Maximum            | Search Minimum
 ------------------------ | ----------------------- | ----------------------- | -----------------------  | -----------------------   | -----------------------
 [Array](https://github.com/shumarb/notes-and-code/tree/main/data-structures/array)         | O(_N_)                    | O(_N_)                    | O(_N_)                     | O(_N_)                      | O(_N_)
-ArrayList | O(_N_)                    | O(_N_)                    | O(_N_)                     | O(_N_)                      | O(_N_)    
-[Binary Max Heap](https://github.com/shumarb/notes-and-code/tree/main/data-structures/binary-max-heap)         | O(_N_ log<sub>2</sub>_N_)      | O(_N_)                    | O(_N_)                     | O(1)                      | O(_N_)
-[Binary Min Heap](https://github.com/shumarb/notes-and-code/tree/main/data-structures/binary-min-heap)         | O(_N_ log<sub>2</sub>_N_)      | O(_N_)                    | O(_N_)                     | O(_N_)                      | O(1)
-[Binary Search Tree](https://github.com/shumarb/notes-and-code/tree/main/data-structures/binary-search-tree)    | O(_N_ log<sub>2</sub>_N_)     | O(_N_)      | O(_N_)      | O(_N_)       | O(_N_)
-[Queue](https://github.com/shumarb/notes-and-code/tree/main/data-structures/queue)                    | O(_N_)                    | O(_N_)                    | O(_N_)                     | O(_N_)                      | O(_N_)   
-[Stack](https://github.com/shumarb/notes-and-code/tree/main/data-structures/stack)                   | O(_N_)                    | O(_N_)                    | O(_N_)                     | O(_N_)                      | O(_N_)   
+[ArrayList](https://github.com/shumarb/notes-and-code/tree/main/data-structures/arraylist)  | O(_N_)                    | O(_N_)                    | O(_N_)                     | O(_N_)                      | O(_N_)
+[Binary Max Heap](https://github.com/shumarb/notes-and-code/tree/main/data-structures/binary-max-heap)         | O(log<sub>2</sub>_N_)      | O(_N_)                    | O(_N_)                     | O(1)                      | O(_N_)
+[Binary Min Heap](https://github.com/shumarb/notes-and-code/tree/main/data-structures/binary-min-heap)         | O(log<sub>2</sub>_N_)      | O(_N_)                    | O(_N_)                     | O(_N_)                      | O(1)
+[Binary Search Tree](https://github.com/shumarb/notes-and-code/tree/main/data-structures/binary-search-tree)    | O(_N_)     | O(_N_)      | O(_N_)      | O(_N_)       | O(_N_)
+[Queue](https://github.com/shumarb/notes-and-code/tree/main/data-structures/queue)                   | O(1)                    | O(1)                   | O(_N_)                     | O(_N_)                      | O(_N_)   
+[Stack](https://github.com/shumarb/notes-and-code/tree/main/data-structures/stack)                   | O(1)                    | O(1)                   | O(_N_)                     | O(_N_)                      | O(_N_)
+[Vector](https://github.com/shumarb/notes-and-code/tree/main/data-structures/vector)  | O(_N_)                    | O(_N_)                    | O(_N_)                     | O(_N_)                      | O(_N_)   
 
 ## How to use this folder
 1. If you have not cloned this repository on your local machine, follow the [instructions](https://github.com/shumarb/notes-and-code#how-to-use-this-repository).
---
+++ data-structures/arraylist/ArrayListExample.java
@@ -0,0 +1,150 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+class ArrayListExample {
+
+    private ArrayList <Integer> arrayList = new ArrayList<>();
+    private Random myRandom = new Random();
+    
+    // Displays Line
+    // Precon: Nil
+    // Postcon: Nil
+    private void displaysLine() {
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------");
+    }
+
+    // Displays New Line
+    // Precon: Nil
+    // Postcon: Nil
+    private void displaysNewLine() {
+        System.out.println();
+    }
+
+    // Forms ArrayList
+    // Precon: ArrayList initialised and has 0 elements
+    // Postcon: ArrayList has >= 1 element
+    private void insertElements() {
+        System.out.println("============ Insertion ============");
+        int numberOfElements = myRandom.nextInt(5, 12);
+        System.out.println("Creating an ArrayList with " + numberOfElements + " elements:");
+        displaysNewLine();
+        for (int i = 0; i < numberOfElements; i++) {
+            int toAdd = myRandom.nextInt(-10, 10);
+            System.out.println(" * Element #" + (i + 1) + ": " + toAdd);
+            arrayList.add(i, toAdd);
+        }
+        displaysNewLine();
+        displaysArrayList("Insertion of " + numberOfElements + " elements complete. ");
+        displaysLine();
+    }
+    
+    // Displays ArrayList
+    // Precon: ArrayList has >= 1 element
+    // Postcon: Execute some operations on ArrayList
+    private void displaysArrayList(String message) {
+        System.out.println(message + "ArrayList: " + arrayList);
+    }
+
+    // Searches for element in ArrayList
+    // Precon: ArrayList has >= 1 element
+    // Postcon: Search for maximum element
+    private void searchesElement() {
+        System.out.println("============ Search ============");
+        displaysArrayList("");
+        displaysNewLine();
+        int key = myRandom.nextInt(-10, 10);
+        if (arrayList.contains(key)) {
+            System.out.println(key + " is in ArrayList");
+        } else {
+            System.out.println(key + " is not in ArrayList");
+        }
+        displaysLine();
+    }
+
+    // Searches for maximum element
+    // Precon: ArrayList has >= 1 element
+    // Postcon: Search for minimum element
+    private void searchesMaximum() {
+        System.out.println("============ Search Maximum ============");
+        displaysArrayList("");
+        int maximumElement = arrayList.get(0);
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) > maximumElement) {
+                maximumElement = arrayList.get(i);
+            }
+        }
+        displaysNewLine();
+        System.out.println("Maximum: " + maximumElement);
+        displaysLine();
+    }
+
+    // Searches for minimum element
+    // Precon: ArrayList has >= 1 element
+    // Postcon: Deletes element from ArrayList
+    private void searchesMinimum() {
+        System.out.println("============ Search Minimum ============");
+        displaysArrayList("");
+        int minimumElement = arrayList.get(0);
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) < minimumElement) {
+                minimumElement = arrayList.get(i);
+            }
+        }
+        displaysNewLine();
+        System.out.println("Minimum: " + minimumElement);
+        displaysLine();
+    }
+
+    // Displays key ArrayList operations
+    // Precon: ArrayList of size >= 1 formed
+    // Postcon: Apply some of these key operations on the ArrayList
+    private void explainsArrayList() {
+        displaysLine();
+        System.out.println("=========== What is an ArrayList? ============");
+        System.out.println("ArrayList is a resizable array");
+        displaysNewLine();
+
+        int i = 1;
+        System.out.println("Key ArrayList operations: ");
+        System.out.println(" " + i++ + ". add(Element e): Add the specified element to end of the ArrayList");
+        System.out.println(" " + i++ + ". add(int index, Element e): Add the specified element at the specified index of ArrayList");
+        System.out.println(" " + i++ + ". clear(): Remove all elements in the ArrayList");
+        System.out.println(" " + i++ + ". contains(Element e): Return true if the ArrayList contains specified element, else return false");
+        System.out.println(" " + i++ + ". get(int index): Retrieve the element at the specified index of the ArrayList. Note: Ensure 0 <= index < size of the ArrayList");
+        System.out.println(" " + i++ + ". isEmpty(): Return true if the ArrayList is empty, else return false");
+        System.out.println(" " + i++ + ". remove(int index): Remove the element at specified index of the ArrayList");
+        System.out.println(" " + i++ + ". set(int index, Element e): Replace the current element at the specified index of ArrayList with the specified element");
+        System.out.println(" " + i + ". size(): Obtain number of the elements in the ArrayList");
+        displaysLine();
+    }
+
+    // Deletes element from ArrayList
+    // Precon: ArrayList has >= 1 element
+    // Postcon: Nil
+    private void deletesElement() {
+        System.out.println("============ Deletion ============");
+        displaysArrayList("Before deletion, ");
+        displaysNewLine();
+
+        int indexOfDeletion = myRandom.nextInt(0, arrayList.size() - 1);
+        System.out.println(" * Deletion at index " + indexOfDeletion + ", which has element " + arrayList.get(indexOfDeletion));
+        arrayList.remove(indexOfDeletion);
+        displaysNewLine();
+
+        displaysArrayList("After deletion, ");
+        displaysLine();
+    }
+
+    private void run() {
+        explainsArrayList();
+        insertElements();
+        searchesElement();
+        searchesMaximum();
+        searchesMinimum();
+        deletesElement();
+    }
+    public static void main(String[] args) {
+        ArrayListExample obj = new ArrayListExample();
+        obj.run();
+    }
+}
