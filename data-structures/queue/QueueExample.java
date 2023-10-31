d1d2f08b4a5a8c7858fb1f5fae296ee98083e41f
Sheikh-Umar <shumarb@outlook.com>
2023-10-31 10:48:31 +0800

    Refactored code on Queue, and updated README

--- data-structures/queue/QueueExample.java
+++ data-structures/queue/QueueExample.java
@@ -4,18 +4,17 @@ import java.util.Random;
 
 class QueueExample {
     
+    private Queue <Integer> myQueue = new LinkedList <> ();
     private Random myRandom = new Random();
-    
+
     // Forms queue of N values from user
     // Precon: N > 0
     // Postcon: Queue of N values formed
-    private Queue <Integer> formsQueue() {
+    private void formsQueue() {
         displaysLine();
-        Queue <Integer> myQueue = new LinkedList <> ();
         for (int i = 0; i < myRandom.nextInt(5, 12); i++) {
             myQueue.offer(myRandom.nextInt(-1000, 1000));
         }
-        return myQueue;
     }
 
     // Displays Line
@@ -24,45 +23,55 @@ class QueueExample {
     private void displaysLine() {
         System.out.println("----------------------------------------------------------------------------------------------------------------------------");
     }
+
+    // Displays new line
+    // Precon: Nil
+    // Postcon: Nil
+    private void displaysNewLine() {
+        System.out.println();
+    }
     
     // Displays key queue operations
     // Precon: myQueue has N values entered from user
     // Postcon: Execute these operations on queue
-    private void displaysKeyQueueOperations(Queue <Integer> myQueue) {
-        int i = 1;
-        System.out.println("Queue: " + myQueue);
+    private void explainsQueue() {
         displaysLine();
-        System.out.println("Queue operates on FIFO principle: First In First Out.");
-        System.out.println("Queue operations to remember: ");
-        System.out.println(i++ + ". offer(element E): Inserts element into queue, or return null if queue is empty, without violating capacity restrictions");
-        System.out.println(i++ + ". peek(): Retrieves, but does not remove, the head of queue, or return null if queue is empty");
-        System.out.println(i++ + ". poll(): Retrieves and removes (dequeuing) head of queue, or return null if queue is empty");
-        System.out.println(i++ + ". isEmpty(): Return true if no element in queue");
-        System.out.println(i++ + ". clear(): Removes all elements in queue");
-        System.out.println(i + ". size(): Returns number of elements in queue");
+        System.out.println("=========== What is a Queue? ============");
+        System.out.println("Queue is a data structure operating on a FIFO principle: First In First Out.");
+        displaysNewLine();
+
+        int i = 1;
+        System.out.println("Key Queue operations: ");
+        System.out.println(" " + i++ + ". offer(element E): Insert element into the queue, or return null if queue is empty, without violating capacity restrictions");
+        System.out.println(" " + i++ + ". peek(): Retrieve, but does not remove, the head of queue, or return null if queue is empty");
+        System.out.println(" " + i++ + ". poll(): Retrieve and remove (dequeuing) the head of queue, or return null if queue is empty");
+        System.out.println(" " + i++ + ". isEmpty(): Return true if no element in the queue");
+        System.out.println(" " + i++ + ". clear(): Remove all the elements in the queue");
+        System.out.println(" " + i + ". size(): Return the total number of elements in the queue");
         displaysLine();
     }
 
     // Executes some operations on queue
     // Precon: Method displaying key operations of queue displayed
     // Postcon: End of program
-    private void executesKeyQueueOperations(Queue <Integer> myQueue) {
+    private void executesKeyQueueOperations() {
         int numberToFind = myRandom.nextInt(-1000, 1000);
         System.out.println("Queue: " + myQueue);
-        System.out.println("Apply some of operations on queue:");
-        System.out.println("* peek(): Top element in queue ----------> " + myQueue.peek());
-        System.out.println("* poll(): To remove ----------> " + myQueue.poll() + " | After poll(), queue: " + myQueue);
-        System.out.println("* contains(Element e): Does queue contain " + numberToFind 
+        displaysNewLine();
+        System.out.println("Apply some key operations on queue:");
+        System.out.println(" * peek(): Top element in the queue ----------> " + myQueue.peek());
+        System.out.println(" * poll(): To remove ----------> " + myQueue.poll() + " | After poll(), queue: " + myQueue);
+        System.out.println(" * contains(Element e): Does queue contain " + numberToFind 
                             + "? ----------> " + myQueue.contains(numberToFind)
-                            + " | myQueue: " + myQueue);
-        System.out.println("* size(): Number of elements in myQueue ----------> " + myQueue.size() + " | myQueue: " + myQueue);
+        );
+        System.out.println(" * size(): Number of elements in myQueue ----------> " + myQueue.size());
         displaysLine();
     }
 
     private void run() {
-        Queue <Integer> myQueue = formsQueue();
-        displaysKeyQueueOperations(myQueue);
-        executesKeyQueueOperations(myQueue);
+        formsQueue();
+        explainsQueue();
+        executesKeyQueueOperations();
     }
 
     public static void main(String[] args) {
--- data-structures/queue/README.md
+++ data-structures/queue/README.md
@@ -6,15 +6,15 @@ A `Queue` is a **linear and dynamic** data structure operating on a **First In, 
 ## Key Operations of Queue
 Operation                | Explanation
 -------------------------| --------------------------------------------------------------------------------------------------------------
-peek()                   | Retrieves, but does not remove, element at top of the `Queue`
-pop()                    | Removes the element at the top of the `Queue`
-push(Element e)          | Pushes element into top of the `Queue`
-clear()                  | Removes all elements in the `Queue`
-isEmpty()                | Returns true only if the `Queue` is empty, else return false
-search(Element e)        | Returns 1-based position of the element in the `Queue`, else return `-1` if the element is not in the `Queue`
-size()                   | Obtains number of elements in the `Queue`
-get(int index)           | Retrieves element at specified `index`. Note: Ensure index < size of the `Queue`
-contains(Element e)      | Returns true if the `Queue` contains specified element, else return false
+peek()                   | Retrieve, but does not remove, the element at top of the `Queue`
+poll()                   | Remove the element at the top of the `Queue`
+offer(Element e)         | Add the element to the top of the `Queue`
+clear()                  | Remove all the elements in the `Queue`
+isEmpty()                | Return true only if the `Queue` is empty, else return false
+search(Element e)        | Return 1-based position of the element in the `Queue`, else return `-1` if the element is not in the `Queue`
+size()                   | Obtain the total number of elements in the `Queue`
+get(int index)           | Retrieve the element at specified `index`. Note: Ensure index < size of the `Queue`
+contains(Element e)      | Return true if the `Queue` contains specified element, else return false
 
 ## How to use this folder
 1. If you have not cloned this repository on your local machine, follow the [instructions](https://github.com/shumarb/notes-and-code#how-to-use-this-repository).
