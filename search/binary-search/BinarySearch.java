f4b4f946fc18367b30801cddafe82ef9890ae38e
Sheikh Umar <shumarb@outlook.com>
2023-11-24 08:44:16 +0800

    Refactored code

--- search/binary-search/BinarySearch.java
+++ search/binary-search/BinarySearch.java
@@ -1,101 +1,125 @@
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 
 class BinarySearch {
-    private Random rand = new Random();
+    
+    private HashSet <Integer> set = new HashSet <> ();
+    private int[] inputArray;
+    private Random myRandom = new Random();
 
-    // Forms array
-    // Precon: Array not instantiated
-    // Postcon: Array of a random size created, and it contains a random set of nubmers
-    private int[] formsArray() {
-        int inputArraySize = rand.nextInt(5, 12);
-        int[] inputArray = new int[inputArraySize];
-        for (int i = 0; i < inputArray.length; i++) {
-            inputArray[i] = rand.nextInt(-10, 10);
+    private void binarySearch() {
+        System.out.println("======= Binary Search =======");
+        displaysArray();
+        
+        for (int i = 0; i < 2; i++) {
+            int key;
+            if (i == 0) {
+                key = formsData(true);
+            } else {
+                key = formsData(false);
+            }
+
+            boolean isKeyFound = false; 
+            ArrayList <Integer> elementsCheckedSet = new ArrayList <> ();
+            int highIndex = inputArray.length - 1;
+            int lowIndex = 0;
+            
+            while (lowIndex <= highIndex) {
+                int midIndex = (highIndex + lowIndex) / 2;
+                int middleElement = inputArray[midIndex];
+                elementsCheckedSet.add(middleElement);
+                if (middleElement == key) {
+                    isKeyFound = true;
+                    break;
+                } else {
+                    if (middleElement < key) {
+                        // Iignore all elements <= middleElement
+                        lowIndex = midIndex + 1;
+                    } else {
+                        // Ignore all elements > middleElement
+                        highIndex = midIndex - 1;
+                    }
+                }
+            }
+
+            displaysNewLine();
+            if (isKeyFound) {
+                System.out.print(" * " + key + " found, ");
+            } else {
+                System.out.print(" * " + key + " not found, ");
+            }
+            System.out.println("elements checked: " + elementsCheckedSet);
         }
-        Arrays.sort(inputArray);
-        return inputArray;
+        displaysLine();
     }
 
-    // Displays line
-    // Precon: Nil
-    // Postcon: Nil
-    private void displaysLine() {
-        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
-    }
-
-    // Displays array
-    // Precon: Program is executing binary search
-    // Postcon: Nil
-    private void displaysArray(int[] inputArray) {
-        System.out.print("** inputArray: [");
+    private void displaysArray() {
+        System.out.print("Array: [");
         for (int i = 0; i < inputArray.length - 1; i++) {
             System.out.print(inputArray[i] + " ");
         }
         System.out.println(inputArray[inputArray.length - 1] + "]");
     }
 
-    // Executes Binary Search
-    // Precon: Number to find not defined
-    // Poston: Prints out if number to find has been found in array
-    private void executesBinarySearch(int[] inputArray) {
-        displaysLine();
-        boolean isTargetFound = false;
-        // lowIndex represents index of element <= middle index of array
-        // highIndex represents index of element > middle index of array 
-        int highIndex = inputArray.length - 1;
-        int indexOfTargetNumber = 0;
-        int lowIndex = 0;
-        int numberOfIterations = 1;
-        int targetNumber = rand.nextInt(-5, 5);
-        while (lowIndex <= highIndex) {
-            int midIndex = (highIndex + lowIndex) / 2;
-            int middleElement = inputArray[midIndex];
-            System.out.println("* Iteration #" + numberOfIterations++);
-            displaysArray(inputArray);
-            System.out.println("** lowIndex: " + lowIndex + ", middleIndex: " + midIndex + ", highIndex: " + highIndex);
-            System.out.println("** targetNumber: " + targetNumber + ", middleElement: " + middleElement);
-            if (middleElement == targetNumber) {
-                indexOfTargetNumber = midIndex;
-                isTargetFound = true;
-                break;
-            } else {
-                if (middleElement < targetNumber) {
-                    // currentElement < targetNumber
-                    // hence ignore all elements <= middleElement
-                    // by setting the low index to be index after middle index
-                    lowIndex = midIndex + 1;
-                    System.out.println("*** Ignore all elements both after and inclusive of index" + midIndex
-                                        + ", next iteration start at index " + (lowIndex)
-                    );
-                } else {
-                    // currentElement >= targetNumber
-                    // hence ignore all elements > middleElement
-                    // by setting the high index to be the index after middle index
-                    highIndex = midIndex - 1;
-                    System.out.println("*** Ignore all elements both after and inclusive of index " + midIndex
-                                        + ", next iteration start at index " + (highIndex)
-                    );
-                }
-            }
-            displaysLine();
-        }
+    private void displaysLine() {
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+    }
 
-        if (isTargetFound) {
-            displaysLine();
-            System.out.println("* Outcome: Found. TargetNumber: " + targetNumber 
-                                + " | inputArray[" + indexOfTargetNumber
-                                + "]: " + inputArray[indexOfTargetNumber]
-            );
-        } else {
-            System.out.println("Outcome: " + targetNumber + " not found.");
+    private void displaysNewLine() {
+        System.out.println();
+    }
+
+    private void formsData() {
+        // Note that myRandom.nextInt(x, y) generates numbers in bound [x, y)
+        // Hence, to generate a number that is inclusive of both x and y: myRandom.nextInt(x, y + 1)
+        int data = formsData(false);
+        int order = myRandom.nextInt(0, 2);
+        for (int i = 0; i < myRandom.nextInt(5, 13); i++) {
+            if (order == 0) {
+                // Descending order of number to be inserted into the Array
+                // because merge sort sorts the elements in ascending order
+                set.add(data--);
+            } else {
+                set.add(formsData(false));
+            }
         }
+    }
+
+    private int formsData(boolean isDataInArray) {
+        int data;
+        if (isDataInArray) {
+            do {
+                data = myRandom.nextInt(-100, 101);
+            } while (!set.contains(data));
+        } else {
+            do {
+                data = myRandom.nextInt(-100, 101);
+            } while (set.contains(data));
+        }
+        return data;
+    }
+
+    private void insertion() {
+        displaysLine();
+        System.out.println("======= Insertion =======");
+        formsData();
+        inputArray = new int[set.size()];
+        System.out.println("Forming an array with " + inputArray.length + " elements:");
+        displaysNewLine();
+        int i = 0;
+        for (int data: set) {
+            System.out.println(" * inserting " + data);
+            inputArray[i++] = data;
+        }
+        Arrays.sort(inputArray);
         displaysLine();
     }
     
     private void run() {
-        int[] inputArray = formsArray();
-        executesBinarySearch(inputArray);
+        insertion();
+        binarySearch();
     }
 
     public static void main(String[] args) {
--- search/linear-search/LinearSearch.java
+++ search/linear-search/LinearSearch.java
@@ -1,83 +1,109 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 
 class LinearSearch {
+
+    private HashSet <Integer> set = new HashSet <> ();
+    private int[] inputArray;
     private Random myRandom = new Random();
 
-    // Forms array
-    // Precon: No inputArray
-    // Postcon: inputArray contains >= 1 element
-    private int[] formsArray() {
-        int arraySize = myRandom.nextInt(5, 12);
-        int[] inputArray = new int[arraySize];
-        for (int i = 0; i < inputArray.length; i++) {
-            inputArray[i] = myRandom.nextInt(-10, 10);
-        }
-        return inputArray;
-    }
-
-    // Executes Linear Search
-    // Precon: inputArray has 5 - 12 elements
-    // Postcon: End of program
-    private void executesLinearSearch(int[] inputArray) {
-        boolean isFound = false;
-        int targetNumber = myRandom.nextInt(-10, 10);
-        int targetNumberIndex = 0;
-
-        displaysLine();
-        System.out.print("inputArray: [");
-        displaysInputArray(inputArray);
-
-        for (int i = 0; i < inputArray.length; i++) {
-            displaysLine();
-            System.out.println("* Iteration " + (i + 1));
-            System.out.print("** inputArray: [");
-            displaysInputArray(inputArray);
-            System.out.println("** Target Number: " + targetNumber 
-                                + " | inputArray[" + i 
-                                + "]: " + inputArray[i]
-            );
-
-            if (inputArray[i] == targetNumber) {
-                targetNumberIndex = i;
-                isFound = true;
-                break;
-            }
-        }
-
-        if (isFound) {
-            displaysLine();
-            System.out.println("Outcome: Found. | Target Number of " 
-                                + targetNumber + " found at index " 
-                                + targetNumberIndex
-            );
-            displaysLine();
-        } else {
-            displaysLine();
-            System.out.println("Outcome: " + targetNumber + " not found.");
-            displaysLine();
-        }
-    }
-
-    // Displays line
-    // Precon: Nil
-    // Postcon: Nil
-    private void displaysLine() {
-        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
-    }
-
-    // Displays input array
-    // Precon: inputArray has 5 - 12 elements
-    // Postcon: Nil
-    private void displaysInputArray(int[] inputArray) {
-        for (int i = 0; i <inputArray.length - 1; i++) {
+    private void displaysArray() {
+        System.out.print("Array: [");
+        for (int i = 0; i < inputArray.length - 1; i++) {
             System.out.print(inputArray[i] + " ");
         }
         System.out.println(inputArray[inputArray.length - 1] + "]");
     }
 
+    private void displaysLine() {
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+    }
+
+    private void displaysNewLine() {
+        System.out.println();
+    }
+
+    private void formsData() {
+        // Note that myRandom.nextInt(x, y) generates numbers in bound [x, y)
+        // Hence, to generate a number that is inclusive of both x and y: myRandom.nextInt(x, y + 1)
+        int data = formsData(false);
+        int order = myRandom.nextInt(0, 2);
+        for (int i = 0; i < myRandom.nextInt(5, 13); i++) {
+            if (order == 0) {
+                // Descending order of number to be inserted into the Array
+                // because merge sort sorts the elements in ascending order
+                set.add(data--);
+            } else {
+                set.add(formsData(false));
+            }
+        }
+    }
+
+    private int formsData(boolean isDataInArray) {
+        int data;
+        if (isDataInArray) {
+            do {
+                data = myRandom.nextInt(-100, 101);
+            } while (!set.contains(data));
+        } else {
+            do {
+                data = myRandom.nextInt(-100, 101);
+            } while (set.contains(data));
+        }
+        return data;
+    }
+
+    private void insertion() {
+        displaysLine();
+        System.out.println("======= Insertion =======");
+        formsData();
+        inputArray = new int[set.size()];
+        System.out.println("Forming an array with " + inputArray.length + " elements:");
+        displaysNewLine();
+        int i = 0;
+        for (int data: set) {
+            System.out.println(" * inserting " + data);
+            inputArray[i++] = data;
+        }
+        displaysLine();
+    }
+    
+    private void linearSearch() {
+        System.out.println("======= Linear Search =======");
+        displaysArray();
+        for (int i = 0; i < 2; i++) {
+            ArrayList <Integer> elementsCheckedSet = new ArrayList <> ();
+            int key;
+            if (i == 0) {
+                key = formsData(true);
+            } else {
+                key = formsData(false);
+            }
+
+            boolean isKeyFound = false;
+            for (int j = 0; j < inputArray.length; j++) {
+                elementsCheckedSet.add(inputArray[j]);
+                if (inputArray[j] == key) {
+                    isKeyFound = true;
+                    break;
+                }
+            }
+
+            displaysNewLine();
+            if (isKeyFound) {
+                System.out.print(" * " + key + " found, ");
+            } else {
+                System.out.print(" * " + key + " not found, ");
+            }
+            System.out.println("elements checked: " + elementsCheckedSet);
+        }
+        displaysLine();
+    }
+
     private void run() {
-        int[] inputArray = formsArray();
-        executesLinearSearch(inputArray);
+        insertion();
+        linearSearch();
 
     }
     public static void main(String[] args) {
