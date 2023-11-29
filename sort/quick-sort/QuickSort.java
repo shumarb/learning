1a4d7e63f6fa2044748b8ecc0fd49d069724e214
Sheikh Umar <shumarb@outlook.com>
2023-11-30 00:24:10 +0800

    Refactored code

--- sort/quick-sort/QuickSort.java
+++ sort/quick-sort/QuickSort.java
@@ -9,27 +9,43 @@ class QuickSort {
     private int[] inputArray;
     private Random myRandom = new Random();
 
-    private void displaysArray(String sentence, int[] inputArray, boolean isDisplaysLine) {
-        System.out.print(sentence);
+    private void displaysArray(String sentence, int[] inputArray, boolean isDisplaysNewLine, boolean isDisplaysLine) {
+        displaysMessage(1, sentence, false, false);
         for (int i = 0; i < inputArray.length - 1; i++) {
-            System.out.print(inputArray[i] + " ");
+            displaysMessage(1, inputArray[i] + " ", false, false);
         }
-        System.out.println(inputArray[inputArray.length - 1] + "]");
+        displaysMessage(1, inputArray[inputArray.length - 1] + "]", true, false);
+        if (isDisplaysNewLine) {
+            displaysNewLine();
+        }
         if (isDisplaysLine) {
             displaysLine();
         }
     }
 
     private void displaysChange() {
-        System.out.println("======= Quick Sort =======");
-        displaysArray(" * Before:\t[", initialArray, false);
-        displaysNewLine();
-        displaysArray(" * After:\t[", inputArray, true);
+        displaysMessage(0, "======= Quick Sort =======", false, false);
+        displaysArray(" * Before:\t[", initialArray, true, false);
+        displaysArray(" * After:\t[", inputArray, false, true);
     }
 
     private void displaysLine() {
         System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
     }
+
+    private void displaysMessage(int messageType, String message, boolean isDisplayNewLine, boolean isDisplayLine) {
+        if (messageType == 0) {
+            System.out.println(message);
+        } else {
+            System.out.print(message);
+        }
+        if (isDisplayNewLine) {
+            displaysNewLine();
+        }
+        if (isDisplayLine) {
+            displaysLine();
+        }
+    }    
 
     private void displaysNewLine() {
         System.out.println();
@@ -67,18 +83,17 @@ class QuickSort {
 
     private void insertion() {
         displaysLine();
-        System.out.println("======= Insertion =======");
+        displaysMessage(0, "======= Insertion =======", false, false);
         formsData();
         inputArray = new int[set.size()];
-        System.out.println("Forming an array with " + inputArray.length + " elements:");
-        displaysNewLine();
+        displaysMessage(0, "Forming an array with " + inputArray.length + " elements:", true, false);
         int i = 0;
         for (int data: set) {
-            System.out.println(" * inserting " + data);
+            displaysMessage(0, " * Insert: " + data, true, false);
             inputArray[i++] = data;
         }
         initialArray = Arrays.copyOf(inputArray, inputArray.length);
-        displaysNewLine();
+        displaysArray("Array: [", inputArray, false, true);
     }
 
     private int partition(int[] inputArray, int lowIndex, int highIndex) {
@@ -106,14 +121,14 @@ class QuickSort {
             // If condition in case leftPointer and rightPointer
             // do not encounter an element > pivot and <= pivot respectively
             if (leftPointer < rightPointer) {
-                swap(inputArray, leftPointer, rightPointer);
+                swap(leftPointer, rightPointer);
             }
         }
 
         // 3. Now, both leftPointer and rightPointer represents 
         // correct index that pivot has to be and highIndex represents current index of pivot
         // because at the start of program, the pivot is the last element of the partition at highIndex
-        swap(inputArray, leftPointer, highIndex);
+        swap(leftPointer, highIndex);
 
         // 4. Return index of pivot, which can be either leftPointer or rightPointer
         // because both pointers are pointing to the index of the partition
@@ -137,7 +152,7 @@ class QuickSort {
         quickSort(inputArray, pivotIndex + 1, highIndex);
     }
 
-    private void swap(int[] inputArray, int firstIndex, int secondIndex) {
+    private void swap(int firstIndex, int secondIndex) {
         int temp = inputArray[firstIndex];
         inputArray[firstIndex] = inputArray[secondIndex];
         inputArray[secondIndex] = temp;
