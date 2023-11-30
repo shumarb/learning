190a6d0c024ebf2ec6a2b1c416842ed3b56c7b0c
Sheikh Umar <shumarb@outlook.com>
2023-12-01 07:28:28 +0800

    Refactored code

--- sort/merge-sort/MergeSort.java
+++ sort/merge-sort/MergeSort.java
@@ -9,12 +9,15 @@ class MergeSort {
     private int[] inputArray;
     private Random myRandom = new Random();
 
-    private void displaysArray(String sentence, int[] inputArray, boolean isDisplaysLine) {
-        System.out.print(sentence);
+    private void displaysArray(String sentence, int[] inputArray, boolean isDisplaysNewLine, boolean isDisplaysLine) {
+        displaysMessage(1, sentence, false, false);
         for (int i = 0; i < inputArray.length - 1; i++) {
-            System.out.print(inputArray[i] + " ");
+            displaysMessage(1, inputArray[i] + " ", false, false);
         }
-        System.out.println(inputArray[inputArray.length - 1] + "]");
+        displaysMessage(1, inputArray[inputArray.length - 1] + "]", true, false);
+        if (isDisplaysNewLine) {
+            displaysNewLine();
+        }
         if (isDisplaysLine) {
             displaysLine();
         }
@@ -24,16 +27,29 @@ class MergeSort {
         System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
     }
 
+    private void displaysMessage(int messageType, String message, boolean isDisplayNewLine, boolean isDisplayLine) {
+        if (messageType == 0) {
+            System.out.println(message);
+        } else {
+            System.out.print(message);
+        }
+        if (isDisplayNewLine) {
+            displaysNewLine();
+        }
+        if (isDisplayLine) {
+            displaysLine();
+        }
+    }
+
     private void displaysNewLine() {
         System.out.println();
     }
     
     private void displaysChange() {
-        System.out.println("======= Merge Sort =======");
-        displaysArray(" * Before:\t[", initialArray, false);
-        displaysNewLine();
-        displaysArray(" * After:\t[", inputArray, true);
-    }
+        displaysMessage(0, "======= Merge Sort =======", false, false);
+        displaysArray(" * Before:\t[", initialArray, true, false);
+        displaysArray(" * After:\t[", inputArray, false, true);
+     }
 
     private void formsData() {
         // Note that myRandom.nextInt(x, y) generates numbers in bound [x, y)
@@ -67,18 +83,17 @@ class MergeSort {
 
     private void insertion() {
         displaysLine();
-        System.out.println("======= Insertion =======");
+        displaysMessage(0, "======= Insertion =======", false, false);
         formsData();
         inputArray = new int[set.size()];
-        System.out.println("Forming an array with " + inputArray.length + " elements:");
-        displaysNewLine();
+        displaysMessage(0, "Forming an array with " + inputArray.length + " elements:", true, false);
         int i = 0;
         for (int data: set) {
-            System.out.println(" * inserting " + data);
+            displaysMessage(0, " * Insert: " + data, true, false);
             inputArray[i++] = data;
         }
         initialArray = Arrays.copyOf(inputArray, inputArray.length);
-        displaysLine();
+        displaysArray("Array:[", inputArray, false, true);
     }
 
     private void merge(int[] inputArray, int[] leftPartitionArray, int[] rightPartitionArray) {
