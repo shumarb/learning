a207e6d85adcdc95c2c83feae4ebfec42aea1dae
S.Umar <shumarb@outlook.com>
2024-09-30 19:42:39 +0800

    Added maximum and minimum variables

--- data-structures/code/BasicOperations.java
+++ data-structures/code/BasicOperations.java
@@ -4,6 +4,8 @@ import java.util.Random;
 public class BasicOperations {
     protected HashSet<Integer> set = new HashSet<> ();
     protected Random myRandom = new Random();
+    protected int maximum = Integer.MAX_VALUE;
+    protected int minimum = Integer.MIN_VALUE;
 
     protected void displaysLine() {
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
